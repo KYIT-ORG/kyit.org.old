2ec9167aea15640a640034441b0f62ba3c486b0e
haha <johndoe@example.com>
2021-11-18 21:17:42 +0800

    include inline css

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,34 @@
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight")
+const CleanCSS = require("clean-css")
+const { DateTime } = require('luxon')
 
 module.exports = function (eleventyConfig) {
 
+    eleventyConfig.setBrowserSyncConfig({
+
+        extensions: ["html"]
+    
+    })
+
+    
     // Return your Object options:
     eleventyConfig.addPlugin(syntaxHighlight)
+
+    eleventyConfig.addFilter("cssmin", function(code) {
+        return new CleanCSS({}).minify(code).styles
+    })
+
+	eleventyConfig.addFilter('postDate', function(dateObj) {
+		return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
+    })
+    
+	eleventyConfig.addFilter('parseURL', function(i) {
+		o = i.split('/')
+		o.shift()
+		o.pop()
+		o.pop()
+		return o
+    })
     
     return {
         dir: {
